bac0b82ea85c7c6373c8173722fe12694579abf2
Mark Wilson <Mark.WILSON7@student.tafesa.edu.au>
2022-08-28 21:18:54 +0930

    Addition of 'Main Menu' form plus accompanying files as well as the 'images' folder (including image files) to the Universal Calculator solution.

---
+++ UniversalCalculator/MainMenu.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainMenu : Page
+	{
+		public MainMenu()
+		{
+			this.InitializeComponent();
+		}
+
+		private void mathsCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void mortgageCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void currencyConverterButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void tripCalculatorButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void exitButton1_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
