ca3231bfcc2493dd2796bd3f7f1938e87f9ef002
000961892 <Hamish.OREILLY@Student.TafeSA.edu.au>
2022-08-30 18:31:40 +0930

    Created & Design TripCalculator.xaml and TripCalculator.xaml.cs

--- UniversalCalculator/App.xaml.cs
+++ UniversalCalculator/App.xaml.cs
@@ -30,7 +30,7 @@ namespace Calculator
 			if (e.PrelaunchActivated == false)
 			{
 				if (rootFrame.Content == null)
-					rootFrame.Navigate(typeof(MainMenu), e.Arguments);
+					rootFrame.Navigate(typeof(TripCalculator), e.Arguments);
 
 				Window.Current.Activate();
 			}
---
+++ UniversalCalculator/TripCalculator.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class TripCalculator : Page
+	{
+		private DateTime dateHired;
+		private double strKm;
+		private double endKm;
+		private int dayHired;
+		private decimal pricePerDay;
+		private decimal total;
+
+		public TripCalculator()
+		{
+			this.InitializeComponent();
+		}
+
+
+
+		private void sysDateButton_Click(object sender, RoutedEventArgs e)
+		{
+			dateHirePicker.SelectedDate = DateTime.Today;
+		}
+
+		private async void calcButton_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				strKm = double.Parse(strKmBox.Text);
+			}
+			catch (Exception)
+			{
+				
+			}
+
+
+		}
+
+		private void exitButtom_Click(object sender, RoutedEventArgs e)
+		{
+			Frame.Navigate(typeof(MainMenu));
+		}
+	}
+}
